c34c5c351d000e438cace1fe7cc61cd0248b6717
jordanistan <jordan.robison@gmail.com>
2020-06-16 19:36:01 -0500

    First commit, script is a simple python to open a a browser to windows93.net

---
+++ giggles.py
@@ -0,0 +1,9 @@
+
+#
+# Created by Jordan
+# Please report any issues where you can't turn down 4 Watt
+# jordan.robison@gmail.com
+# This script helps you turn down 4 Watt
+import webbrowser
+
+webbrowser.open('https://tinyurl.com/y3he56dv') 
